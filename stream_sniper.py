5e0483a7eba9b3e000d67afb1436eee7f89faf23
luisa-wx <luisahealey@gmail.com>
2026-02-01 21:23:14 -0500

    Add files via upload

--- stream_sniper.py
+++ stream_sniper.py
@@ -908,7 +908,7 @@ class StreamingCall:
                     # The final result will have the correct full number.
                     combined = self.transcript_window + " " + transcript_chunk
                     
-                    logger.info(f"[DG:interim] chunk={transcript_chunk!r}")
+                    logger.debug(f"[DG:interim] chunk={transcript_chunk!r}")
                     
                     temp_c, temp_f = parse_temperature(combined)
                     if temp_c is not None:
